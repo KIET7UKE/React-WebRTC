565b8e7e45981e988401ded6da0ec02fb9be4b92
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2024-10-02 19:46:10 +0530

    Update SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io("localhost:8000"), []);
+    const socket = useMemo(() => io("https://react-webrtc-backend.onrender.com/"), []);
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
